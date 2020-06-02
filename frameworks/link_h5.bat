a1da38c34b5d1e0234fa751ddb0412b5b2182bca
dzw <dzwwgy@163.com>
2020-06-02 23:17:17 +0800

    lnk

--- frameworks/link_h5.bat
+++ frameworks/link_h5.bat
@@ -1,3 +1,3 @@
-if exist cocos2d-html5 do rmdir
+if exist cocos2d-html5 rmdir cocos2d-html5 
 
-mklink /D cocos2d-html5 L:\CPJXHD\ProjDemo_2020-05-29\src\frameworks\cocos2d-html5
+mklink /D cocos2d-html5 F:\cocos2d-x-3.17.2\web
